ed8368b38ca64f259726dfb79878f56798e98475
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-17 16:45:19 +0000

    route helper methods done

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
   #get("/", { :controller => "movies", :action => "index" })
-  get({"/" => "movies#index"})
+  root "movies#index"
   # Routes for the Movie resource:
 
   # CREATE
-  post({"/movies"=>  "movies#create"})
-  get({"/movies/new" => "movies#new" })
+  post"/movies"=>  "movies#create", as: :movies
+  get"/movies/new" => "movies#new", as: :new_movie
           
   # READ
-  get({"/movies"=> "movies#index" })
-  get({"/movies/:id"=> "movies#show" })
+  get"/movies"=> "movies#index"
+  get"/movies/:id"=> "movies#show", as: :movie
+  #how to name a route, use details_path(id) to call details_url(id) gives the entire url
   
   # UPDATE
-  patch({"/movies/:id"=> "movies#update" })
-  get({"/movies/:id/edit" => "movies#edit" })
+  patch"/movies/:id"=> "movies#update"
+  get"/movies/:id/edit" => "movies#edit", as: :edit_movie
   
   # DELETE
-  delete({"/movies/:id"=> "movies#destroy" })
+  delete "/movies/:id"=> "movies#destroy"
 
   #------------------------------
 end
